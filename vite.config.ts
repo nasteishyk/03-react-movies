c8d89491d00611f6c35e76a7dc40ba61c8cfaaa2
Anastasiia <nastasiia.kletsko@gmail.com>
2025-11-08 23:47:31 +0200

    changed content of vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
-})
+  plugins: [react()],
+  build: {
+    sourcemap: true,
+  },
+});
