987cc3d95da9236a42f52820fb3e9af2057111de
Anastasiia <nastasiia.kletsko@gmail.com>
2025-11-08 23:45:26 +0200

    1st commit

---
+++ src/services/movieService.ts
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+import type { Movie } from "../types/movie";
+
+interface MoviesHttpResponse {
+  page: number;
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+}
+
+export const handleFetch = async (query: string): Promise<Movie[]> => {
+  const response = await axios.get<MoviesHttpResponse>(
+    "https://api.themoviedb.org/3/search/movie",
+    {
+      params: {
+        api_key: import.meta.env.VITE_TMDB_API_KEY,
+        query: query,
+      },
+      headers: {
+        Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
+      },
+    }
+  );
+  return response.data.results;
+};
